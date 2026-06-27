c1ca554e0669df73d2b9fdc1d844429bbd500037
Dinesh Rajan <dineshrajan2112@gmail.com>
2026-06-27 10:31:19 +0530

    Update logo to new typographic design

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -46,7 +46,7 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          Logo
+          <img src="/images/suyog_logo.png" alt="Suyog Logo" style={{ height: '40px', objectFit: 'contain' }} />
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
